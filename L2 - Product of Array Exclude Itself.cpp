17938c472b0218fff71c4b64cc89f1249f6ba724
Kevin Wu <xdewtr@gmail.com>
2016-07-04 16:05:07 -0700

    L2 - Product fixed since sum needs to be large enough

--- L2 - Product of Array Exclude Itself.cpp
+++ L2 - Product of Array Exclude Itself.cpp
@@ -4,7 +4,26 @@ public:
      * @param A: Given an integers array A
      * @return: A long long array B and B[i]= A[0] * ... * A[i-1] * A[i+1] * ... * A[n-1]
      */
-    vector<long long> productExcludeItself(vector<int> &nums) {
+    vector<long long> productExcludeItself(vector<int> &v) {
         // write your code here
+        /*
+			B[i] = (A[0]*...*A[i-1]) * (A[i+1]*...*A[n-1])
+			so we process left first and later we process right
+
+        */
+		vector<long long> res(v.size(),1);
+		long long sum = 1; // need to be large enough to hold the number
+		if(v.size()==0) return res;
+		for(int i=0; i<v.size(); ++i){
+			res[i]*= sum;	// so we don't multiply ourself
+			sum*=v[i];
+		}
+		sum = 1;
+		for(int i=v.size()-1; i>=0; --i){
+			res[i]*= sum;
+			sum*=v[i];
+		}
+		return res;
+
     }
 };
